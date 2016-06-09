6fed3f7c94db12eee77b1594ba40e9fb79e9d816
Rehan Dalal <rehandalal@gmail.com>
2016-06-09 16:17:16 -0400

    Add flake8 and pre-commit

--- morgoth/wsgi.py
+++ morgoth/wsgi.py
@@ -1,17 +1,8 @@
-"""
-WSGI config for morgoth project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "morgoth.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-
-from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
