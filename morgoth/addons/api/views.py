4a9a4b4245436adb5a7931eaf09616c58e303d77
Rehan Dalal <rehandalal@gmail.com>
2016-07-05 14:36:39 -0400

    API views for Addons

---
+++ morgoth/addons/api/views.py
@@ -0,0 +1,47 @@
+from rest_framework import permissions
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from morgoth.addons.api.serializers import AddonSerializer, AddonGroupSerializer
+from morgoth.addons.models import Addon, AddonGroup
+
+
+class AddonViewSet(ModelViewSet):
+    queryset = Addon.objects.all()
+    serializer_class = AddonSerializer
+    permission_classes = [
+        permissions.DjangoModelPermissionsOrAnonReadOnly,
+    ]
+
+    @detail_route(methods=['GET'])
+    def groups(self, request, *args, **kwargs):
+        addon = self.get_object()
+        serializer = AddonGroupSerializer(addon.groups.all(), many=True)
+        return Response(serializer.data)
+
+
+class AddonGroupViewSet(ModelViewSet):
+    queryset = AddonGroup.objects.all()
+    serializer_class = AddonGroupSerializer
+    permission_classes = [
+        permissions.DjangoModelPermissionsOrAnonReadOnly,
+    ]
+
+    @detail_route(methods=['POST'])
+    def add_addons(self, request, *args, **kwargs):
+        group = self.get_object()
+        addon_ids = request.data.get('addon_ids', '').split(',')
+        addons = Addon.objects.filter(id__in=addon_ids)
+
+        for addon in addons:
+            group.addons.add(addon)
+
+    @detail_route(methods=['POST'])
+    def remove_addons(self, request, *args, **kwargs):
+        group = self.get_object()
+        addon_ids = request.data.get('addon_ids', '').split(',')
+        addons = Addon.objects.filter(id__in=addon_ids)
+
+        for addon in addons:
+            group.addons.remove(addon)
---
+++ morgoth/addons/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+
+from rest_framework.routers import DefaultRouter
+
+from morgoth.addons.api.views import (
+    AddonViewSet,
+    AddonGroupViewSet,
+)
+
+# API Router
+router = DefaultRouter()
+router.register(r'addon', AddonViewSet)
+router.register(r'addon_group', AddonGroupViewSet)
+
+urlpatterns = [
+    url(r'^api/v1/', include(router.urls)),
+]
