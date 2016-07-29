1bb6d763ddb23a4a87dfb3a68d3a1129bba91922
Rehan Dalal <rehandalal@gmail.com>
2016-07-29 07:06:25 -0400

    Move client side files out of Django project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var BundleTracker = require('webpack-bundle-tracker');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [
   {
@@ -8,20 +8,20 @@ module.exports = [
 
     entry: {
       main: [
-        './morgoth/base/static/js/index.js',
-        './morgoth/base/static/sass/style.scss'
-      ]
+        './client/ui/js/index.js',
+        './client/ui/sass/style.scss',
+      ],
     },
 
     output: {
-        path: path.resolve('./assets/bundles/'),
-        filename: '[name]-[hash].js',
-        chunkFilename: '[id].bundle.js'
+      path: path.resolve('./assets/bundles/'),
+      filename: '[name]-[hash].js',
+      chunkFilename: '[id].bundle.js',
     },
 
     plugins: [
       new BundleTracker({ filename: './webpack-stats.json' }),
-      new ExtractTextPlugin('[name]-[hash].css')
+      new ExtractTextPlugin('[name]-[hash].css'),
     ],
 
     module: {
@@ -29,17 +29,17 @@ module.exports = [
         {
           test: /(\.|\/)(jsx|js)$/,
           exclude: /node_modules/,
-          loader: 'babel'
+          loader: 'babel',
         },
         {
           test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass?sourceMap')
+          loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass?sourceMap'),
         },
         {
           test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-          loader: 'file-loader'
-        }
-      ]
-    }
-  }
+          loader: 'file-loader',
+        },
+      ],
+    },
+  },
 ];
