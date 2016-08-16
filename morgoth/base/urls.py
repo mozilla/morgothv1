7c34e0726e1f579f6f156639a8ae99581c218871
Rehan Dalal <rehandalal@gmail.com>
2016-08-16 11:54:53 -0400

    Improve catch-all regular expression

--- morgoth/base/urls.py
+++ morgoth/base/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 from morgoth.base import views
 from morgoth.base.api.views import UserViewSet
 
+
 # API Router
 router = DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -12,5 +13,6 @@ router.register(r'user', UserViewSet)
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
     url(r'^logout/$', views.log_out, name='logout'),
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?!(api)|(admin)).+$', views.index),
 ]
