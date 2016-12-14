6382726530f28dadcf6a5f10b82052068ee1be71
Rehan Dalal <rehandalal@gmail.com>
2016-12-14 12:34:03 -0500

    Fix react dependencies

--- client/ui/index.jsx
+++ client/ui/index.jsx
@@ -5,9 +5,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import routes from './routes';
 import configureStore from './stores/configureStore';
+
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 
 const store = configureStore();
